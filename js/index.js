028886fc18f6c06db6f20dfd8910c5a4357359ab
Raphael Mayer <35350266+raphaelmayer@users.noreply.github.com>
2019-06-08 17:29:16 +0200

    various improvements
    added loading anim for feedback
    cleaned up js
    played around with animations a bit

--- js/index.js
+++ js/index.js
@@ -1,71 +1,110 @@
+// 
+// 
+
+// jsonData for switching between °C & °F
+var jsonData;   
+getGeoLocation();
+
+// adding EventListeners to all buttons
+$("#fahrenheit").click(convertCelFar);
+$("#celsius").click(convertCelFar);
+$(".form-button").click(searchWeather);
+
+
+// ---------------------------------
+//            FUNCTIONS
+// ---------------------------------
+
 function getGeoLocation() {     
   if("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(getData);
+    navigator.geolocation.getCurrentPosition(getWeather);
   } else {
     alert("GeoLocation not available. Check if you enabled GeoLocation.");
   };
 };
 
-var jsonData ;
+// weather by coords, if geolocation is enabled
+function getWeather(position){
+  const searchTerm = `${position.coords.latitude},${position.coords.longitude}`;
 
-function getData(position){
-  const searchTerm = position.coords.latitude + "," + position.coords.longitude;
+  // animate();
 
-  $.getJSON("https://api.apixu.com/v1/forecast.json?key=b7c8d803ca1543d2a71222120180204&q=" + searchTerm + "&days=5", function(data, status, xhr) {
+  $.getJSON(`https://api.apixu.com/v1/forecast.json?key=b7c8d803ca1543d2a71222120180204&q=${searchTerm}&days=5`, function(data, status, xhr) {
     jsonData = data;
     appendData(data);
   });
 };
 
+// weather by region or city name
+function searchWeather(e) {
+  e.preventDefault();
+  const searchTerm = e.target.form[0].value;
+
+  // animate();
+  $("#loading").show().animate({ width: "50%" }, 500);
+
+  $.getJSON(`https://api.apixu.com/v1/forecast.json?key=b7c8d803ca1543d2a71222120180204&q=${searchTerm}&days=5`, function(data, status, xhr) {
+    jsonData = data;
+    appendData(data);
+  });
+}
+
 function appendData(data) {
+  // animate();
+  $("#loading").animate({ width: "100%" }, 500, () => $("#loading").hide().css("width", "0%"));
+
   $("#icon").attr("src", "https:" + data.current.condition.icon);
   $("#weatherDesc").empty().append(data.current.condition.text);
   $("#temp").empty().append(data.current.temp_c + "°C");
   $("#city").empty().append(data.location.name + ", " + data.location.country);
-  $("#windSpeed").empty().append('<i class="wi wi-small-craft-advisory"></i>' + data.current.wind_kph + " km/h");
-  $("#windDeg").empty().append('<i class="wi wi-wind-direction"></i>' + data.current.wind_degree + "°");
-  $("#humidity").empty().append('<i class="wi wi-humidity"></i>' + data.current.humidity + " %");
-  $("#pressure").empty().append('<i class="wi wi-barometer"></i>' + data.current.pressure_mb + " hPa");
-  checkCelOrFahr();
+  $("#windSpeed").empty().append(`<i class="wi wi-small-craft-advisory"></i>${data.current.wind_kph} km/h`);
+  $("#windDeg").empty().append(`<i class="wi wi-wind-direction"></i>${data.current.wind_degree}°`);
+  $("#humidity").empty().append(`<i class="wi wi-humidity"></i>${data.current.humidity} %`);
+  $("#pressure").empty().append(`<i class="wi wi-barometer"></i>${data.current.pressure_mb} hPa`);
+  appendForecast();
 };
+
+function appendForecast(format) {
+  const forecasts = jsonData.forecast.forecastday;
+  format = format || "c";
+
+  $(".forecast-container").empty();
+  for(let i=0;i<5;i++) {
+    $(".forecast-container").append(`
+      <div class="forecast-box">
+        <div class="forecast-temp">${forecasts[i].day["maxtemp_" + format]}°${format.toUpperCase()}</div>
+        <img style="width: 100%" src="https:${forecasts[i].day.condition.icon}"/>
+        <div class="forecast-temp">${forecasts[i].day["mintemp_" + format]}°${format.toUpperCase()}</div>
+        <div class="forecast-box-day">${getDay(i)}</div>
+      </div>
+    `);
+  };
+}
 
 function getDay(i) {
   const x = new Date(jsonData.current.last_updated_epoch*1000);
   const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sa", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sa"];
   const dayStr = days[x.getDay() + 1 + i];
+  
   return dayStr;
 }
 
-function checkCelOrFahr(){
-  const arr = jsonData.forecast.forecastday;
-  $(".forecast-container").empty();
-  if ($("#celsius").is(":checked")) {
-    $("#temp").empty().append(jsonData.current.temp_c + "°C");
-    for(let i=0;i<5;i++) {
-      $(".forecast-container").append('<div class="forecast-box">' + arr[i].day.maxtemp_c + '°C<img style="width: 100%" src="https:' + arr[i].day.condition.icon + '"/><div class="forecast-temp">' +arr[i].day.mintemp_c + '°C</div><div class="forecast-box-day">' + getDay(i) + '</div></div>');
-    };
-  } else {
-    $("#temp").empty().append(jsonData.current.temp_f + "°F");
-    for(let i=0;i<5;i++) {
-      $(".forecast-container").append('<div class="forecast-box">' + arr[i].day.maxtemp_f + '°F<img style="width: 100%" src="https:' + arr[i].day.condition.icon + '"/><div class="forecast-temp">' +arr[i].day.mintemp_f + '°F</div><div class="forecast-box-day">' + getDay(i) + '</div></div>');
-    };
-  }
+function convertCelFar(){
+  const format = $("#celsius").is(":checked") ? "c" : "f";
+
+  $("#temp").empty().append(`${jsonData.current["temp_" + format]}°${format.toUpperCase()}`);
+  appendForecast(format);
 };
 
-
-getGeoLocation();
-
-//celToFahr
-$("#fahrenheit").on("click", checkCelOrFahr);
-$("#celsius").on("click", checkCelOrFahr);
-
-
-//searchbar fetch
-$(".form-button").on("click", function(e) {
-  e.preventDefault();
-  let searchTerm = e.target.form[0].value;
-  $.getJSON("https://api.apixu.com/v1/forecast.json?key=b7c8d803ca1543d2a71222120180204&q=" + searchTerm + "&days=5", function(data, status, xhr) {
-    jsonData = data;
-    appendData(data);
-  });
-});
+// test; unused / commented out
+function animate() {
+  $("#icon").hideToggle();
+  $("#weatherDesc").hideToggle();
+  $("#temp").hideToggle();
+  $("#city").hideToggle();
+  $("#windSpeed").hideToggle();
+  $("#windDeg").hideToggle();
+  $("#humidity").hideToggle();
+  $("#pressure").hideToggle();
+  $(".forecast-container").hideToggle();
+}
